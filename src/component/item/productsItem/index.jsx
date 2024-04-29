c36f81887fdde7eb7f8b52fe49fc5c1b48e140be
pokly0908 <pokly0908@naver.com>
2024-04-29 13:11:23 +0900

    :recycle: 주문하기 버튼을 누르면 상세조회 api 날림

--- src/component/item/productsItem/index.jsx
+++ src/component/item/productsItem/index.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { productItemStyle, productItemNameStyle, productItemPriceStyle } from "./style";
 import Modal from 'react-modal';
 import { apiClient } from "../../../api/client";
@@ -9,6 +9,19 @@ const ProductsItem = ({ product }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [quantity, setQuantity] = useState(1);
   const [address, setAddress] = useState("");
+  const [productDetail, setProductDetail] = useState(null); // 상품 상세 정보를 저장할 상태
+
+  useEffect(() => {
+    if (modalIsOpen) {
+      // 모달이 열릴 때 상품의 상세 정보를 가져옵니다.
+      const fetchProductDetail = async () => {
+        const response = await apiClient.get(`https://9oods.store/api/v1/sellers/${product.domainName}/products/${product.productId}`);
+        setProductDetail(response.data);
+      };
+
+      fetchProductDetail();
+    }
+  }, [modalIsOpen]);
 
   const handleBuyClick = async () => {
     const order = {
@@ -40,16 +53,21 @@ const ProductsItem = ({ product }) => {
       <p css={productItemPriceStyle}>{product.productPrice}</p>
       <button onClick={() => setModalIsOpen(true)}>구매하기</button>
       <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-        <h2>{product.productName}</h2>
-        <p>{product.productPrice}</p>
-        <a>수량  </a>
-        <input type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} placeholder="수량" />
-        <br />
-        <a>주소  </a>
-        <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="주소" />
-        <br />
-        <button onClick={handleBuyClick}>주문하기</button>
-        <button onClick={() => setModalIsOpen(false)}>닫기</button>
+        {productDetail && (
+          <>
+            <h2>{productDetail.productName}</h2>
+            <p>{productDetail.productPrice}</p>
+            <p>{productDetail.productDescription}</p>
+            <a>수량 </a>
+            <input type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} placeholder="수량" />
+            <br />
+            <a>주소 </a>
+            <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="주소" />
+            <br />
+            <button onClick={handleBuyClick}>주문하기</button>
+            <button onClick={() => setModalIsOpen(false)}>닫기</button>
+          </>
+        )}
       </Modal>
     </div>
   );
